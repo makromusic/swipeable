bc7367dc82f28f2aeeb966bd89189f7263ab7009
Metehan Gül <83433955+metehng@users.noreply.github.com>
2023-07-14 14:46:24 +0300

    run swipe func when swiping complete

--- lib/swipeable.dart
+++ lib/swipeable.dart
@@ -1,22 +1,13 @@
 library swipeable;
 
-import 'dart:math' as math;
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 
 class Swipeable extends StatefulWidget {
-  final Widget child;
-  final Widget background;
-  final VoidCallback? onSwipeStart;
-  final VoidCallback? onSwipeLeft;
-  final VoidCallback? onSwipeRight;
-  final VoidCallback? onSwipeCancel;
-  final VoidCallback? onSwipeEnd;
-  final double threshold;
-
-  Swipeable({
+  const Swipeable({
     required this.child,
-    required this.background,
+    required this.backgroundWidget,
     this.onSwipeStart,
     this.onSwipeLeft,
     this.onSwipeRight,
@@ -25,30 +16,45 @@ class Swipeable extends StatefulWidget {
     this.threshold = 64.0,
   });
 
-  State<StatefulWidget> createState() {
-    return _SwipeableState();
-  }
+  final Widget child;
+  final Widget backgroundWidget;
+  final VoidCallback? onSwipeStart;
+  final VoidCallback? onSwipeLeft;
+  final VoidCallback? onSwipeRight;
+  final VoidCallback? onSwipeCancel;
+  final VoidCallback? onSwipeEnd;
+  final double threshold;
+
+  @override
+  State<StatefulWidget> createState() => _SwipeableState();
 }
 
 class _SwipeableState extends State<Swipeable> with TickerProviderStateMixin {
   late AnimationController _moveController;
   late Animation<Offset> _moveAnimation;
 
-  double _dragExtent = 0.0;
-  bool _pastLeftThreshold = false;
-  bool _pastRightThreshold = false;
+  late double _backgroundWidgetOpacity;
 
+  late double _dragExtent;
+
+  bool get _swipingRight => _dragExtent > 0 && widget.onSwipeRight != null;
+  bool get _swipingLeft => _dragExtent < 0 && widget.onSwipeLeft != null;
+
+  @override
   void initState() {
     super.initState();
-    _moveController =
-        AnimationController(duration: Duration(milliseconds: 200), vsync: this);
-    _moveAnimation = Tween<Offset>(begin: Offset.zero, end: Offset(1.0, 0.0))
-        .animate(_moveController);
+    _moveController = AnimationController(
+        duration: const Duration(milliseconds: 200), vsync: this);
+    _moveAnimation =
+        Tween<Offset>(begin: Offset.zero, end: const Offset(1.0, 0.0))
+            .animate(_moveController);
 
-    var controllerValue = 0.0;
-    _moveController.animateTo(controllerValue);
+    _dragExtent = 0.0;
+    _backgroundWidgetOpacity = 0.0;
+    _moveController.animateTo(0.0);
   }
 
+  @override
   void dispose() {
     _moveController.dispose();
     super.dispose();
@@ -65,62 +71,60 @@ class _SwipeableState extends State<Swipeable> with TickerProviderStateMixin {
     final width = context.size!.width;
     if (width == 0) return;
 
-    var delta = details.primaryDelta ?? 0;
-    var oldDragExtent = _dragExtent;
+    final delta = details.primaryDelta ?? 0;
+    final oldDragExtent = _dragExtent;
     _dragExtent += delta;
 
     if (oldDragExtent.sign != _dragExtent.sign) {
-      setState(() {
-        _updateMoveAnimation();
-      });
+      setState(_updateMoveAnimation);
     }
 
-    var movePastThresholdPixels = widget.threshold;
-    var newPos = _dragExtent.abs() / width;
+    final movePastThresholdPixels = widget.threshold;
+    double newPos = _dragExtent.abs() / width;
 
     if (_dragExtent.abs() > movePastThresholdPixels) {
       // how many "thresholds" past the threshold we are. 1 = the threshold 2
       // = two thresholds.
-      var n = _dragExtent.abs() / movePastThresholdPixels;
+      final n = _dragExtent.abs() / movePastThresholdPixels;
 
       // Take the number of thresholds past the threshold, and reduce this
       // number
-      var reducedThreshold = math.pow(n, 0.3);
+      final reducedThreshold = pow(n, 0.3);
 
-      var adjustedPixelPos = movePastThresholdPixels * reducedThreshold;
+      final adjustedPixelPos = movePastThresholdPixels * reducedThreshold;
       newPos = adjustedPixelPos / width;
+    }
 
-      if (_dragExtent > 0 && !_pastLeftThreshold) {
-        _pastLeftThreshold = true;
+    setState(() {
+      _backgroundWidgetOpacity = min(_dragExtent.abs() / widget.threshold, 1);
+    });
+    _moveController.value = newPos;
+  }
 
+  void _handleDragEnd(DragEndDetails details) {
+    setState(() {
+      _backgroundWidgetOpacity = 0;
+    });
+
+    final movePastThresholdPixels = widget.threshold;
+
+    if (_dragExtent.abs() > movePastThresholdPixels) {
+      if (_swipingRight) {
         if (widget.onSwipeRight != null) {
           widget.onSwipeRight?.call();
         }
-      }
-      if (_dragExtent < 0 && !_pastRightThreshold) {
-        _pastRightThreshold = true;
-
+      } else if (_swipingLeft) {
         if (widget.onSwipeLeft != null) {
           widget.onSwipeLeft?.call();
         }
       }
     } else {
-      // Send a cancel event if the user has swiped back underneath the
-      // threshold
-      if (_pastLeftThreshold || _pastRightThreshold) {
-        if (widget.onSwipeCancel != null) {
-          widget.onSwipeCancel?.call();
-        }
+      if (widget.onSwipeCancel != null) {
+        widget.onSwipeCancel?.call();
       }
-      _pastLeftThreshold = false;
-      _pastRightThreshold = false;
     }
 
-    _moveController.value = newPos;
-  }
-
-  void _handleDragEnd(DragEndDetails details) {
-    _moveController.animateTo(0.0, duration: Duration(milliseconds: 200));
+    _moveController.animateTo(0.0, duration: const Duration(milliseconds: 200));
     _dragExtent = 0.0;
 
     if (widget.onSwipeEnd != null) {
@@ -129,28 +133,37 @@ class _SwipeableState extends State<Swipeable> with TickerProviderStateMixin {
   }
 
   void _updateMoveAnimation() {
-    var end = _dragExtent.sign;
-    _moveAnimation =
-        Tween<Offset>(begin: Offset(0.0, 0.0), end: Offset(end, 0.0))
-            .animate(_moveController);
+    final sign = _dragExtent.sign;
+    final end = _swipingRight
+        ? max(0.0, sign)
+        : _swipingLeft
+            ? min(0.0, sign)
+            : 0.0;
+
+    _moveAnimation = Tween<Offset>(begin: Offset.zero, end: Offset(end, 0.0))
+        .animate(_moveController);
   }
 
+  @override
   Widget build(BuildContext context) {
-    var children = <Widget>[
-      widget.background,
-      SlideTransition(
-        position: _moveAnimation,
-        child: widget.child,
-      ),
-    ];
-
     return GestureDetector(
       onHorizontalDragStart: _handleDragStart,
       onHorizontalDragUpdate: _handleDragUpdate,
       onHorizontalDragEnd: _handleDragEnd,
       behavior: HitTestBehavior.opaque,
       child: Stack(
-        children: children,
+        alignment: _swipingRight ? Alignment.centerLeft : Alignment.centerRight,
+        children: [
+          if (_swipingRight || _swipingLeft)
+            AnimatedOpacity(
+                opacity: _backgroundWidgetOpacity,
+                duration: const Duration(milliseconds: 200),
+                child: widget.backgroundWidget),
+          SlideTransition(
+            position: _moveAnimation,
+            child: widget.child,
+          ),
+        ],
       ),
     );
   }
